948b12a3c573016a4a065db30d42dc785d4fa4c7
Amish Mittal <amishmittal@outlook.com>
2025-03-12 20:58:50 +0530

    add adsense id

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9123105121789937" crossOrigin="anonymous"></script>
+      </head>
       <body>
         <AntdRegistry>
           <AntdConfigProvider>
