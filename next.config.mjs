3c2d92de550e00af53d255fb532026a901d870f9
Amish Mittal <amishmittal@outlook.com>
2025-03-24 17:25:39 +0530

    remove static exporting of all files. Back to ISR. :)

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+    // output: 'export',
     // experimental: {
     //     swcPlugins: [['fluentui-next-appdir-directive', { paths: ['@griffel', '@fluentui'] }]],
     // },
