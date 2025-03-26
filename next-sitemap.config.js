0e9631ce92807b5fab4d0235cd82a53898a6620c
Amish Mittal <amishmittal@outlook.com>
2025-03-27 03:38:46 +0530

    update sitemap to www and daily update

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://indiainresearch.org',
+    siteUrl: process.env.SITE_URL || 'https://www.indiainresearch.org',
     generateRobotsTxt: true, 
     generateIndexSitemap: false,
-    changefreq: 'monthly'
+    changefreq: 'daily'
   }
