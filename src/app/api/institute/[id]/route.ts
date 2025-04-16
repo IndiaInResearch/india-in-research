c62010fb8507451fbc4e769130b415ad15113474
Amish Mittal <amishmittal@outlook.com>
2025-04-16 00:10:40 +0000

    get institute information from supabase

--- src/app/api/institute/[id]/route.ts
+++ src/app/api/institute/[id]/route.ts
@@ -1,13 +1,18 @@
 import { get_institute_data_in_mem } from "@/utils/domain-handlers";
+import { supabaseClient } from "@/utils/supabase-client";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: NextRequest, {params}: {params: {id: string}}){
 
-    const result = get_institute_data_in_mem("https://openalex.org/" + params.id)
+    const { data, error } = await supabaseClient.from('institutes').select('*').eq('openalex_id', "https://openalex.org/" + params.id).limit(1)
 
-    if (!result) {
+    if (error) {
+        return new NextResponse("Internal Server Error", { status: 500 })
+    }
+
+    if (!data || data.length == 0) {
         return new NextResponse(JSON.stringify({ error: 'Institute not found' }), { status: 404 });
     }
 
-    return new NextResponse(JSON.stringify(result), { status: 200 });
+    return new NextResponse(JSON.stringify(data[0]), { status: 200 });
 }
